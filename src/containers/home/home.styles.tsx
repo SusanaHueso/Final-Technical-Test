872062900c256cc0dc961ba3ae24e0222ef8d637
SusanaHueso <susana.huesodevis@xing.com>
2022-12-15 23:12:09 +0100

    added login and register buttons at home(not functional right now), added animation to home

--- src/containers/home/home.styles.tsx
+++ src/containers/home/home.styles.tsx
@@ -1,47 +1,76 @@
+import { Button } from "react-bootstrap";
 import styled, { css } from "styled-components";
 import image from "../../images/image.jpg";
 
+// *** Types ***
+export type AnimationType = {
+  transition: string;
+  transform: string;
+};
+
 // *** Base ***
 const baseBeneathHome = css`
-  height: 100%;
-  width: 100%;
-  background-size: cover;
+  height: 100vh;
+  width: 100vw;
+  background-size: fit;
   display: flex;
   flex-wrap: wrap;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   background: linear-gradient(
-      rgba(255, 255, 255, 0.1),
-      rgba(255, 255, 255, 0.1)
+      rgba(255, 255, 255, 0.09),
+      rgba(255, 255, 255, 0.09)
     ),
-    url(${image}); ;
+    url(${image});
+  overflow: hidden;
 `;
+
 const baseHome = css`
-  height: 90%;
-  width: 90%;
+  -webkit-filter: drop-shadow(12px 12px 7px rgba(0, 0, 0, 0.5));
+  height: 90vh;
+  width: 90vw;
   background-image: url(${image});
   background-size: cover;
-  &:hover {
-    background-image: linear-gradient(
-        to bottom,
-        rgba(255, 25, 0, 0.2),
-        rgba(0, 0, 255, 0.4)
-      ),
-      url(${image});
-  }
   border-radius: 13px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-content: center;
+  transition: 1s all;
+`;
+
+const baseStyledButton = css`
+  font-size: 23px;
+  color: white;
+  border-radius: 13px;
+  background-color: #006275;
+  opacity: 70%;
+  border-color: grey;
+  &:hover {
+    transition: transform 2s;
+    transform: rotate3d(5, -1, -1, -1turn);
+  }
 `;
 
 // *** Components ***
 const BeneathHome = styled.div`
   ${baseBeneathHome}
 `;
-const Home = styled.div`
+const Home = styled.div<AnimationType>`
   ${baseHome};
+  &:hover {
+    transition: ${(props: any) => props.transition} !important;
+    transform: ${(props: any) => props.transform} !important;
+  }
+`;
+
+const StyledButton = styled(Button)`
+  ${baseStyledButton};
 `;
 
 export const Styled = {
   BeneathHome,
   Home,
+  Button: StyledButton,
 };
--- src/containers/home/home.tsx
+++ src/containers/home/home.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
 import { Styled } from "./home.styles";
 
 export const Home = () => {
+  const [animation, setAnimation] = useState({
+    transition: "transform 2s",
+    transform: "rotate3d(5, -1, -1, 1turn)",
+  });
+  /* useEffect(() => {
+    setAnimation({
+      transition: "",
+      transform: "",
+    });
+  }, []);*/
+
   return (
     <Styled.BeneathHome>
-      <Styled.Home> </Styled.Home>
+      <Styled.Home {...animation}>
+        <Styled.Button>Login</Styled.Button>{" "}
+        <Styled.Button>Register</Styled.Button>
+      </Styled.Home>
     </Styled.BeneathHome>
   );
 };
