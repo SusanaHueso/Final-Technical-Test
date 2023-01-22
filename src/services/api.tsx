f9622c99aad5c9f2bfff47e45582e9f043712d96
SusanaHueso <susana.huesodevis@xing.com>
2023-01-22 16:59:30 +0100

    added try catch to api call

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,2 +1,18 @@
 import axios from "axios";
-export default axios.create({ baseURL: " http://localhost:3006" });
+export const api = axios.create({ baseURL: " http://localhost:3006" });
+// Api getters
+export const retrieveUsers = async () => {
+  try {
+    return await api.get<string[]>("/users");
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const retrieveBooks = async () => {
+  try {
+    return await api.get("/books");
+  } catch (error) {
+    console.log(error);
+  }
+};
+ 
